15d47e70d304943c10cb95e0382d2d7908d9a9fc
futamiyuuki <aa805766@gmail.com>
2017-09-05 18:57:47 -0700

    Lecture 24. Note App Complete.

--- notes-node/app.js
+++ notes-node/app.js
@@ -3,7 +3,30 @@ const _ = require('lodash');
 const yargs = require('yargs');
 const notes = require('./notes.js');
 
-const argv = yargs.argv;
+const titleOptions = {
+  describe: 'Title of note',
+  demand: true,
+  alias: 't'
+};
+const bodyOptions = {
+  describe: 'Body of note',
+  demand: true,
+  alias: 'b'
+};
+const argv = yargs
+  .command('add', 'Add a new note', {
+    title: titleOptions,
+    body: bodyOptions
+  })
+  .command('list', 'List all notes')
+  .command('read', 'Read a note', {
+    title: titleOptions
+  })
+  .command('remove', 'Remove a note', {
+    title: titleOptions
+  })
+  .help()
+  .argv;
 const command = argv._[0];
 if(command === 'add') {
   const note = notes.addNote(argv.title, argv.body);
