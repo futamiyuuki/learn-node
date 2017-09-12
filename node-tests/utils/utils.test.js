78a53a4c978f9869011daeee7603b5e6d08174c1
futamiyuuki <aa805766@gmail.com>
2017-09-11 17:57:54 -0700

    Lecture 54

--- node-tests/utils/utils.test.js
+++ node-tests/utils/utils.test.js
@@ -3,14 +3,39 @@ const utils = require('./utils');
 
 it('should add two numbers', () => {
   const res = utils.add(33, 11);
-  expect(res).toBe(44);
+  expect(res).toBe(44).toBeA('number');
+});
+
+it('should async add two numbers', done => {
+  utils.asyncAdd(4, 3, sum => {
+    expect(sum).toBe(7).toBeA('number');
+    done();
+  })
 });
 
 it('should square a number', () => {
   const res = utils.square(13);
-  expect(res).toBe(169);
+  expect(res).toBe(169).toBeA('number');
 });
 
-it('should expect some values', () => {
-  //expect({name: 'Andrew'}).toEqual({name: 'Andrew'});
+it('should async square a number', done => {
+  utils.asyncSquare(13, res => {
+    expect(res).toBe(169).toBeA('number');
+    done();
+  });
 });
+
+it('should set first and last names', () => {
+  const user = {
+    age: 25,
+    location: 'China'
+  };
+  expect(utils.setName(user, 'Zedong Mao')).toInclude({
+    lastName: 'Mao',
+    firstName: 'Zedong'
+  }).toBeA('object');
+});
+
+// it('should expect some values', () => {
+//   //expect({name: 'Andrew'}).toEqual({name: 'Andrew'});
+// });
