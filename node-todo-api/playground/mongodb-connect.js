fa91faaa463d4f05dc0058faeb63677520f095da
futamiyuuki <aa805766@gmail.com>
2017-09-13 18:31:26 -0700

    Lecture 66

--- node-todo-api/playground/mongodb-connect.js
+++ node-todo-api/playground/mongodb-connect.js
@@ -1,4 +1,4 @@
-const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
 
 MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
   if(err) return console.log('Unable to connect to MongoDB server');
@@ -12,14 +12,14 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
   //   console.log(JSON.stringify(result.ops, null, 2));
   // });
 
-  db.collection('Users').insertOne({
-    name: 'Tzuyu Chou',
-    age: 18,
-    location: 'Seoul'
-  }, (err, result) => {
-    if(err) return console.log('Unable to insert user', err);
-    console.log(JSON.stringify(result.ops, undefined, 2));
-  });
+  // db.collection('Users').insertOne({
+  //   name: 'Tzuyu',
+  //   age: 18,
+  //   location: 'Seoul'
+  // }, (err, result) => {
+  //   if(err) return console.log('Unable to insert user', err);
+  //   console.log(JSON.stringify(result.ops, undefined, 2));
+  // });
 
   db.close();
 });
