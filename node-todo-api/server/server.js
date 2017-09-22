c3d1add47b12f3f97de550f5a343e7de1aa94ad0
futamiyuuki <aa805766@gmail.com>
2017-09-21 23:52:18 -0700

    Lecture 90

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -1,3 +1,5 @@
+require('./config/config');
+
 const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -8,7 +10,7 @@ const {Todo} = require('./models/todo');
 const {User} = require('./models/user');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 app.use(bodyParser.json());
 
@@ -69,6 +71,17 @@ app.patch('/todos/:id', (req, res) => {
     if(!todo) return res.status(404).send();
     res.send({todo});
   }).catch(err => res.status(400).send());
+});
+
+app.post('/users', (req, res) => {
+  const body = _.pick(req.body, ['email', 'password']);
+  const user = new User(body);
+
+  user.save().then(() => {
+    return user.generateAuthToken();
+  }).then(token => {
+    res.header('x-auth', token).send(user);
+  }).catch(err => res.status(400).send(err));
 });
 
 app.listen(port, () => {
