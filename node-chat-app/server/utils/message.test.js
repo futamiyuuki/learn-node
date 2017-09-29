ed01065d952875b832af38f8b6fd42d081e6eaa0
futamiyuuki <aa805766@gmail.com>
2017-09-28 22:14:47 -0700

    Lecture 115

--- node-chat-app/server/utils/message.test.js
+++ node-chat-app/server/utils/message.test.js
@@ -1,6 +1,6 @@
 const expect = require('expect');
 
-const {generateMessage} = require('./message');
+const {generateMessage, generateLocationMessage} = require('./message');
 
 describe('generateMessage', () => {
   it('should generate correct message object', () => {
@@ -8,6 +8,17 @@ describe('generateMessage', () => {
     const text = 'Lalala';
     const res = generateMessage(from, text);
     expect(res).toMatchObject({from, text});
+    expect(typeof res.createdAt).toBe('number');
+  });
+});
+
+describe('generateLocationMessage', () => {
+  it('should generate correct location object', () => {
+    const from = 'Nayeon';
+    const lat = 123;
+    const long = 321;
+    const res = generateLocationMessage(from, lat, long);
+    expect(res).toMatchObject({from, url: `https://www.google.com/maps?q=${lat},${long}`});
     expect(typeof res.createdAt).toBe('number');
   });
 });
